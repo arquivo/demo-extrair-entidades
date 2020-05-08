ead3cca6d0b3b20936742e636567efa30c212840
André Mourão <andre.mourao@fccn.pt>
2020-05-08 11:11:56 +0100

    Added support for custom URLs

--- README.md
+++ README.md
@@ -20,7 +20,23 @@ python -m spacy download pt_core_news_sm
 ## Executar
 
 ```
-python extrair_entidades.py
+python extrair_entidades.py [url a pesquisar]
+```
+
+Por exemplo, este comando vai extrair entidades da última versão arquivada da página "arquivo.pt":
+```
+$ python extrair_entidades.py arquivo.pt
+Miscellaneous entities, e.g. events, nationalities, products or works of art: 
+Arquivo.pt?; Campeonato Europeu de Futebol 2004; Expo '98; Investigação; O Jogo; Sobre   Pesquisa avançada; Twitter Facebook Subscreva RSS; Área de imprensa Código-aberto & APIs Termos; Últimas novidades
+
+Named person or family.: 
+Acesso Recolha; Colabore Sugira; Formações Contacto; José Saramago Prémio da Literatura; Luís Figo Figo; Notícia do jornal "; Pesquise
+
+Non-GPE locations, mountain ranges, bodies of water: 
+Lisboa
+
+Companies, agencies, institutions, etc.: 
+Crie
 ```
 
 ## Contacto
--- extrair_entidades.py
+++ extrair_entidades.py
@@ -1,10 +1,28 @@
 import spacy
 import requests
+import sys
 from collections import defaultdict
 
 nlp = spacy.load("pt_core_news_sm")
 
-r = requests.get("https://arquivo.pt/textextracted?m=http://www.igot.ulisboa.pt/20181109014435")
+url = sys.argv[1]
+
+r = requests.get("https://arquivo.pt/textsearch?versionHistory={}&maxItems=50".format(url))
+
+results = r.json()["response_items"]
+
+linkToExtractedText = None
+
+for r in results:
+	if r["linkToExtractedText"]:
+		linkToExtractedText = r["linkToExtractedText"]
+		break
+
+if not linkToExtractedText:
+	print("No results for URL \"{}\"".format(url))
+	sys.exit(1)
+
+r = requests.get(linkToExtractedText)
 r.encoding = "utf-8"
 document = r.text
 doc = nlp(document)
@@ -15,5 +33,5 @@ for entity in doc.ents:
 
 for label in entities:
     print(spacy.explain(label) + ": ")
-    print("; ".join(entities[label]))
+    print("; ".join(sorted(entities[label])))
     print()
